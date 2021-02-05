2cfec87dd0addc5c256fc836cfa191c25ba6aa24
Jesse Adelman <jesse@boldandbusted.com>
2021-02-05 18:37:23 -0500

    Add extras/, with shell scripts that bring up services in a K8s cluster.

---
+++ extras/README.md
@@ -0,0 +1,5 @@
+# Extras
+
+Files in this directory could be used to create example deployments, demos, etc.
+
+Quality may vary.
--- extras/airflow.sh
+++ extras/airflow.sh
@@ -2,6 +2,7 @@
 . ./dashboard.sh
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm install my-release bitnami/airflow
+sleep 60
 kubectl port-forward service/my-release-airflow 8443:8080 --address=0.0.0.0 &
 export AIRFLOW_PASSWORD=$(kubectl get secret --namespace default my-release-airflow -o jsonpath="{.data.airflow-password}" | base64 --decode)
 echo Password: $AIRFLOW_PASSWORD 
