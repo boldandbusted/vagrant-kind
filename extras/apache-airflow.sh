42c94631517c62533c25be8e70e748f89c997b0e
Jesse Adelman <jesse@boldandbusted.com>
2021-12-23 18:57:39 -0500

    Disable Airflow chart due to webserver login problems. See #8.

--- extras/apache-airflow.sh
+++ extras/apache-airflow.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-## ERRORS currently on postgresql pod...
-## 2021-12-22 01:33:09.756 GMT [613] FATAL: password authentication failed for user "bn_airflow"
-## 2021-12-22 01:33:09.756 GMT [613] DETAIL: Password does not match for user "bn_airflow".
-## Connection matched pg_hba.conf line 1: "host all all 0.0.0.0/0 md5"
-##
+echo "This chart doesn't work at the moment. See https://github.com/boldandbusted/vagrant-kind/issues/8 ."
+exit 1
 
 . ./lib/dashboard.sh
 
-helm repo add bitnami https://charts.bitnami.com/bitnami
-helm install --atomic my-release bitnami/airflow
-kubectl port-forward service/my-release-airflow 8443:8080 --address=0.0.0.0 >> /dev/null &
-my_ip=$(ip -j -p addr show eth1 | jq -r .[].addr_info[0].local)
+helm repo add apache-airflow https://airflow.apache.org/
+helm install my-airflow apache-airflow/airflow --version 1.3.0 \
+  --set config.core.load_examples=true \
+  --set postgresql.postgresqlUsername="postgres" \
+  --set postgresql.postgresqlPassword="postgres"
+kubectl port-forward service/my-airflow-airflow 8443:8080 --address=0.0.0.0 >> /dev/null &
+#my_ip=$(ip -j -p addr show eth1 | jq -r .[].addr_info[0].local)
 echo "Browse to https://kind.test:8443/ or https://$my_ip:8443/ for Apache Airflow interface."
-export AIRFLOW_PASSWORD=$(kubectl get secret --namespace default my-release-airflow -o jsonpath="{.data.airflow-password}" | base64 --decode)
+export AIRFLOW_PASSWORD=$(kubectl get secret --namespace default my-airflow-airflow -o jsonpath="{.data.airflow-password}" | base64 --decode)
 echo "Airflow password: $AIRFLOW_PASSWORD"
